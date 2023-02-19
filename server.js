1954e9eba131b0397a874b37e85dbffcae88231f
Ethan Kies <ethankies27@gmail.com>
2023-02-19 17:30:12 -0500

    Update server.js

--- server.js
+++ server.js
@@ -16,6 +16,13 @@ mongoose.connect(dbURI, {
   useUnifiedTopology: true
 });
 
+//Handle HTTP requests
+app.get('https://us-east-1.aws.data.mongodb-api.com/app/data-prhca/endpoint/data/v1', (req, res) => {
+    const data = { foo: 'bar', baz: 'qux' };
+    res.json(data);
+  });
+  
+
 const connection = mongoose.connection;
 connection.once('open', () => {
   console.log('MongoDB database connection established successfully');
