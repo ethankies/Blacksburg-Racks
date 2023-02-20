235ef3c76fd4c4faba196b4e37d7bd3c358eadb4
Ethan Kies <ethankies27@gmail.com>
2023-02-19 19:23:26 -0500

    fix

--- MapComponent.js
+++ MapComponent.js
@@ -20,8 +20,8 @@ function MapComponent() {
             />
             <Marker position={position} />
             <Popup>
-        A pretty CSS3 popup. <br /> Easily customizable.
-      </Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
         </Map>
     );
 }
--- server.js
+++ server.js
@@ -16,17 +16,17 @@ async function main() {
 
         //print all databases
         await listDatabases(client);
- 
+
         // Handle HTTP requests
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
+        app.get('/', (req, res) => {
+            res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+        });
         // Start the server
         const port = process.env.PORT || 5000;
         app.listen(port, () => {
             console.log(`Server is running on port ${port}`);
         });
-        
+
     } catch (e) {
 
         console.error(e);
